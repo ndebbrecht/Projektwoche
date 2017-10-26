8a1c8c40280f57086e46ff0524b12c27793d88fc
andrsaks <andreas.skas@gmail.com>
2017-10-26 15:03:46 +0200

    abc

--- src/pages/create-info/create-info.ts
+++ src/pages/create-info/create-info.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 import { InfoNodeProvider } from '../../providers/info-node/info-node';
+import { PosArrayProvider } from '../../providers/pos-array/pos-array';
 
 /**
  * Generated class for the CreateInfoPage page.
@@ -16,7 +17,7 @@ import { InfoNodeProvider } from '../../providers/info-node/info-node';
 })
 export class CreateInfoPage {
 
-  constructor(public viewCtrl: ViewController, public navCtrl: NavController, public navParams: NavParams, public info: InfoNodeProvider) {
+  constructor(public viewCtrl: ViewController, public navCtrl: NavController, public navParams: NavParams, public info: InfoNodeProvider, public posArray: PosArrayProvider) {
   }
 
   title: String;
@@ -28,7 +29,7 @@ export class CreateInfoPage {
   }
 
   addNode() {
-    this.info.addInfoNode(50,50,50,parseInt(this.radius),this.title,this.desc);
+    this.info.addInfoNode(this.posArray.getObject(this.posArray.arrayLength()-1).x,this.posArray.getObject(this.posArray.arrayLength()-1).y,this.posArray.getObject(this.posArray.arrayLength()-1).z,parseInt(this.radius),this.title,this.desc);
     console.log(this.radius);
     this.viewCtrl.dismiss();
   }
